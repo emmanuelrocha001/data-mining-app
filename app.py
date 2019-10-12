0ad34eadef318999a0929b14dc23df15e5b29528
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2019-10-12 09:19:55 -0500

    added route for search

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for
 app = Flask( __name__ )
 
 @app.route('/')
+@app.route('/search')
 def search_engine():
     return render_template( 'index.html' )
 
